19089d3f995d12cd5023605ed5858b70ad9f2ea9
alexanderjcurry <94728576+alexanderjcurry@users.noreply.github.com>
2024-06-03 14:39:19 -0400

    Update create-minecraft-servers.sh
    Added better questionnaire

--- create-minecraft-servers.sh
+++ create-minecraft-servers.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#need to change it to also prompt what number the server order has started at.
 
 NAMESPACE=minecraft
 
+echo "If you want a fresh wipe, run the refresh_script.sh first"
+
 # Prompt for the number of servers to create
 read -p "Enter the number of Minecraft servers to create: " NUM_SERVERS
+
+# Prompt for the starting server number
+read -p "Enter the starting server number (e.g., 1): " START_SERVER_NUM
 
 # Prompt for the starting NodePort
 read -p "Enter the starting NodePort (e.g., 30005 for the next available port after 30004): " START_PORT
@@ -12,9 +16,13 @@ read -p "Enter the starting NodePort (e.g., 30005 for the next available port af
 # Create the namespace if it doesn't exist
 kubectl get namespace $NAMESPACE || kubectl create namespace $NAMESPACE
 
-for i in $(seq 1 $NUM_SERVERS); do
-  SERVER_NUM=$((START_PORT - 30000 + i))
-  NODE_PORT=$((START_PORT + i - 1))
+# Apply the config files
+kubectl apply -f minecraft-env-config.yml
+kubectl apply -f minecraft-config.yml
+
+for i in $(seq 0 $(($NUM_SERVERS - 1))); do
+  SERVER_NUM=$((START_SERVER_NUM + i))
+  NODE_PORT=$((START_PORT + i))
 
   # Replace placeholders in deployment template
   cat deployment-template.yml | sed "s/{{SERVER_NUM}}/$SERVER_NUM/g" | kubectl apply -f -
